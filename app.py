d3eed1b77f8be93b85bd77214c60d6c3d066b4cc
Ubuntu <ubuntu@ip-172-31-3-120.us-east-2.compute.internal>
2025-05-29 05:39:09 +0000

    Blueprint

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask import Flask
 from routes.main import main
+from routes.about import about
 
 app = Flask(__name__)
-    
-app.register_blueprint(main, url_prefix='/')
+app.register_blueprint(main)
+app.register_blueprint(about)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4000)
